253f793c099c17cf45da910d16a06ff668014062
Nizhgo <8900800700a@gmail.com>
2023-01-04 17:01:39 +0300

    saving the selected theme

--- src/scripts/getUserDefaultTheme.ts
+++ src/scripts/getUserDefaultTheme.ts
@@ -1,4 +1,8 @@
 const getUserDefaultTheme = () => {
+	  const userTheme = window.localStorage.getItem('theme');
+	  if (userTheme && ['light', 'dark'].includes(userTheme)) {
+		  	    return userTheme;
+	  }
 	  const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
   return isDarkMode ? 'dark' : 'light';
 }

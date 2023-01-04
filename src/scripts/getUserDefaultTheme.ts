9d566e8a760e2b3ad1f7f5c20ca03371f708de24
Nizhgo <8900800700a@gmail.com>
2023-01-04 17:16:52 +0300

    new favicon + theme fix

--- src/scripts/getUserDefaultTheme.ts
+++ src/scripts/getUserDefaultTheme.ts
@@ -1,10 +1,12 @@
-const getUserDefaultTheme = () => {
+import Theme from "../service/types/Theme";
+
+const getUserDefaultTheme = (): Theme => {
 	  const userTheme = window.localStorage.getItem('theme');
-	  if (userTheme && ['light', 'dark'].includes(userTheme)) {
-		  	    return userTheme;
+	  if (userTheme && ['light', 'dark', 'auto'].includes(userTheme)) {
+		  	    return userTheme as Theme;
 	  }
 	  const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  return isDarkMode ? 'dark' : 'light';
+  return isDarkMode ? 'dark' : 'light' as Theme;
 }
 
 export default getUserDefaultTheme;
